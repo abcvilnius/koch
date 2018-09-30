5bfe5b75e0071eb7e6e65fdbe2e5e3e0452c6096
abcvilnius <abcvilnius@gmail.com>
2018-09-30 11:19:24 +0300

    Update gcd.java

--- gcd.java
+++ gcd.java
@@ -1,13 +1,30 @@
-public int egcd(int a, int b) {
-    if (a == 0)
-        return b;
+import java.util.*;
+public class Greatest_common_Divisor
+{
 
-    while (b != 0) {
-        if (a > b)
-            a = a - b;
-        else
-            b = b - a;
+    public static void main (String[]args){
+            Scanner input = new Scanner(System.in);
+    System.out.println("Enter two integers: ");
+    int n1 = input.nextInt();
+    int n2 = input.nextInt();
+
+    int d = 0;
+    int e=0;
+    int temp = 0;
+    //finds the lowest value
+    if(n1 < n2) {
+        temp = n1;
+        n1 = n2;
+        n2 = temp;
     }
 
-    return a;
+    for(d = n2;(n2%d!=0 || n1%d!= 0);d--){
+
+
+    }
+
+    System.out.println("The Greatest Common Divisor of " + n1 + " and " + n2 + " is " + d);
+
+    }
+
 }
